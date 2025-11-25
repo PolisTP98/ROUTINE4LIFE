b5bcbe3f5e72e8017c48c97962d448473b77df8c
PolisTP98 <123047038@upq.edu.mx>
2025-11-25 05:14:30 -0600

    Update config.py

--- routine4life_movil_api/config.py
+++ routine4life_movil_api/config.py
@@ -5,11 +5,11 @@ import pyodbc
 # ------------------------------------------
 
 # PARÁMETROS DE LA CONEXIÓN
-DRIVER = "{ODBC Driver 18 for SQL Server}"
+DRIVER = "{ODBC Driver 17 for SQL Server}"
 SERVER = "POLISTP98"
 DATABASE = "routine4life"
-Trusted_Connection = True
-Encrypt = True
+USERNAME = "r4l.polistp98"
+PASSWORD = "polistp98"
 TrustServerCertificate = True
 
 # CADENA DE TEXTO DE CONEXIÓN
@@ -17,8 +17,8 @@ CONNECTION_STRING = (
     f"DRIVER={DRIVER};"
     f"SERVER={SERVER};"
     f"DATABASE={DATABASE};"
-    f"Trusted_Connection={'yes' if Trusted_Connection else 'no'};"
-    f"Encrypt={'yes' if Encrypt else 'no'};"
+    f"UID={USERNAME};"
+    f"PWD={PASSWORD};"
     f"TrustServerCertificate={'yes' if TrustServerCertificate else 'no'};"
 )
 
