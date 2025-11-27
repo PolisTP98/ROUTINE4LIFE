356ee76afb502fec9cc5b22fd910f884f63cf41e
PolisTP98 <123047038@upq.edu.mx>
2025-11-27 17:11:46 -0600

    Update queries.ts

--- routine4life/src/services/queries.ts
+++ routine4life/src/services/queries.ts
@@ -4,14 +4,14 @@ import {apiGet} from "./api";
 // | CONSULTAS DE LA API QUE SE EJECUTARÁN EN EL DISPOSITICO MÓVIL |
 // -----------------------------------------------------------------
 
-// INTERFAZ PARA TIPAR LA RESPUESTA DE LA API
+// INTERFAZ PARA TIPAR LA RESPUESTA DE LA API (ConnectionStatus)
 export interface ConnectionStatus {
   status: string;
   database_connection: string;
   message: string;
 }
 
-// SERVICIO QUE CONSULTA LA RUTA DE FLASK
+// CONSULTAR LA RUTA DE FLASK
 export async function checkConnection(): Promise<ConnectionStatus> {
   return await apiGet<ConnectionStatus>("/connectionStatus");
 }
