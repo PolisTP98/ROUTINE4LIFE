bc087ef48bf3b5002c42ea25b22d70af6c623b6d
mhaHub <123046406@upq.edu.mx>
2026-03-30 14:35:32 -0600

    Cambios 2

--- shared/database.py
+++ shared/database.py
@@ -8,28 +8,28 @@ from sqlalchemy.engine import URL
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-# 🔹 URL de conexión (SQL Server con usuario y contraseña)
+#URL de conexión 
 DATABASE_URL = "mssql+pyodbc://@DESKTOP-6RRSB8S\\SQLEXPRESS01/routine4life?driver=ODBC+Driver+17+for+SQL+Server"
 
 
-# 🔹 Engine
+# Engine
 engine = create_engine(
     DATABASE_URL,
     echo=False,
     pool_pre_ping=True
 )
 
-# 🔹 Base
+# Base
 Base = declarative_base()
 
-# 🔹 Sesiones
+#Sesiones
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
     bind=engine
 )
 
-# 🔹 Función para obtener sesión
+# Función para obtener sesión
 def get_db():
     db = SessionLocal()
     try:
